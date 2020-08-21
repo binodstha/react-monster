876f27486f6f213eb6aba2ed53d5f934116a66ce
binod <ekbana@ekbanas-Mac-mini.local>
2020-08-21 09:32:40 +0545

    Updates

---
+++ precache-manifest.e1035693a5b3ff746ebaab431c7d7522.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d80e685e33cc5af9aa27436e44788f",
+    "url": "/react-monster/index.html"
+  },
+  {
+    "revision": "341661ccd1311eff8e98",
+    "url": "/react-monster/static/css/main.9e70b623.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/react-monster/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "341661ccd1311eff8e98",
+    "url": "/react-monster/static/js/main.49430a14.chunk.js"
+  },
+  {
+    "revision": "bb3f04e8bde7a5ae2bc9",
+    "url": "/react-monster/static/js/runtime-main.4e3149a6.js"
+  }
+]);
